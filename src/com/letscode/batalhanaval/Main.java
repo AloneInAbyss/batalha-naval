dbeea3996fbb9a8eb6145327c401daaced666d56
aloneinabyss <thiago-assi@outlook.com>
2022-01-10 18:04:50 -0300

    reset do código

--- src/com/letscode/batalhanaval/Main.java
+++ src/com/letscode/batalhanaval/Main.java
@@ -2,15 +2,6 @@ package com.letscode.batalhanaval;
 
 public class Main {
     public static void main(String[] args) {
-        int qtdPecas = 4;
-        int qtdColunas = 10;
-        char[] nomeLinhas = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
 
-        Jogo jogo = new Jogo(qtdPecas, qtdColunas, nomeLinhas);
-        jogo.inicializarJogo();
-
-//        while (!jogo.fimDeJogo) {
-//            jogo.rodada();
-//        }
     }
 }
